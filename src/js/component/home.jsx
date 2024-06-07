de991188eb8799aa916fe48af00b47cf0131a0c4
Cristinacbm5 <157827345+Cristinacbm5@users.noreply.github.com>
2024-06-07 16:31:49 +0000

    Cristina

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,93 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const crearUsuario = () => {
+fetch('https://playground.4geeks.com/todo/users/cristina', {
+  method: "POST"
+  }
+)
+.then(resp => resp.json())
+.then(data => console.log(data))
+.catch(error => console.log(error));
+
+}
+
+  useEffect(() => {
+    getTodos()
+  }, []);
+  const getTodos = () => {
+    fetch('https://playground.4geeks.com/todo/users/cristina')
+  .then(resp => {
+    if (resp.status === 404) {
+    crearUsuario()
+    }
+    return resp.json()})
+  .then(data => {
+    setTodos(data.todos)
+    console.log(data.todos)
+  })
+  .catch(error => console.log(error))};
+ 
+
+  const deleteTodo = (id) => {
+    fetch(`https://playground.4geeks.com/todo/todos/${id}`,{
+      method: "DELETE",
+            })
+      .then(resp => {
+        if (resp.status === 200) {
+        getTodos()
+        }
+        return resp.json()})
+      .then(data => console.log(data))
+      .catch(error => console.log(error))}
+  ;
+ 
+  const updateTodos = (newTodos) => {
+    fetch('https://playground.4geeks.com/todo/todos/{todo_id}', {
+      method: "PUT",
+      body: JSON.stringify(newTodos),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(resp => resp.json())
+    .then(data => setTodos(data))
+    .catch(error => console.log(error));
+  };
+
+  const clearTodos = () => {
+    fetch('https://playground.4geeks.com/todo/user/', {
+      method: "PUT",
+      body: JSON.stringify([]), 
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(resp => resp.json())
+    .then(data => setTodos([]))
+    .catch(error => console.log(error));
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos?.map((todo, index) => (
+          <li key={index}>
+            {todo.label}
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => clearTodos()}>Clear All</button>
+    </div>
+  );
 };
 
-export default Home;
+export default TodoApp;
+
+
+
+      
